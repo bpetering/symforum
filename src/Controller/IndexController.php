0d2177def6f6713fbdd540347b6ac220f95bb67d
Ben Petering <benjamin.petering@gmail.com>
2020-10-26 20:15:40 +1030

    Add basic code to display subforums on main page

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -5,16 +5,20 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\SubforumRepository;
 
 class IndexController extends AbstractController
 {
     /**
      * @Route("/", name="index")
      */
-    public function index(): Response
+    public function index(SubforumRepository $subforumRepository): Response
     {
+        $subforums = $subforumRepository->findAll();
+
         return $this->render('index/index.html.twig', [
             'controller_name' => 'IndexController',
+            'subforums' => $subforums
         ]);
     }
 }
