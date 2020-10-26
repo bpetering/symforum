2c32bf8b1f88e7c99a0f3c0934b06e7fb6ea2626
Ben Petering <benjamin.petering@gmail.com>
2020-10-26 21:02:07 +1030

    Add author_id to threads migration

--- migrations/Version20201026101719.php
+++ migrations/Version20201026101719.php
@@ -20,12 +20,12 @@ final class Version20201026101719 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         $this->addSql('
-            INSERT INTO "thread" (id, subforum_id, topic)
+            INSERT INTO "thread" (id, subforum_id, topic, author_id)
             VALUES
-            (1, 1, \'Should PHP be a compiled language?\'),
-            (2, 3, \'New things to expect in ES2029\'),
-            (3, 2, \'SSSSSSsssssssSSSSSSSss\'),
-            (4, 4, \'Symfony and the mobile web\')
+            (1, 1, \'Should PHP be a compiled language?\', 2),
+            (2, 3, \'New things to expect in ES2029\', 3),
+            (3, 2, \'SSSSSSsssssssSSSSSSSss\', 3),
+            (4, 4, \'Symfony and the mobile web\', 4)
             ;
         ');
     }
