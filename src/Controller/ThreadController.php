37ce5319f10bb0817b0ca1558c19b653956d2c8a
Ben Petering <benjamin.petering@gmail.com>
2020-10-26 22:53:14 +1030

    Thread controller loads the thread

--- src/Controller/ThreadController.php
+++ src/Controller/ThreadController.php
@@ -5,16 +5,18 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Thread;
 
 class ThreadController extends AbstractController
 {
     /**
-     * @Route("/thread", name="thread")
+     * @Route("/thread/{id}", name="thread")
      */
-    public function index(): Response
+    public function index(Thread $thread): Response
     {
         return $this->render('thread/index.html.twig', [
             'controller_name' => 'ThreadController',
+            'thread' => $thread
         ]);
     }
 }
