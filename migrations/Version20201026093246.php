e17732329a1852f025133a49231297e618d75014
Ben Petering <benjamin.petering@gmail.com>
2020-10-26 20:06:26 +1030

    Migration - create some forums

---
+++ migrations/Version20201026093246.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20201026093246 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        $this->addSql('
+            INSERT INTO "subforum" (id, name)
+            VALUES 
+            (1, \'PHP\'),
+            (2, \'Python\'),
+            (3, \'Javascript\'),
+            (4, \'Web design\')
+            ;
+        ');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+    }
+}
